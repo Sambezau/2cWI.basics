d9faba7a9573c813320c4ea7c9099520f2d71a7a
Samuel <samuel.meusburger@student.htldornbirn.at>
2021-12-09 14:38:20 +0100

    JS03 finished

---
+++ JS/JS/script1.js
@@ -0,0 +1,92 @@
+console.log("Hallo");
+console.log("wild");
+
+// 01 Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+let d = 2;
+
+console.log("A hat den Wert " + a);
+
+
+
+
+// 01 Rechnungsarten
+// *, /, +, -
+
+
+
+Ergebnis = a + b + c;
+console.log(Ergebnis);
+
+Ergebnis2 = c / d
+console.log(Ergebnis2);
+
+Ergebnis3 = b * c;
+console.log(Ergebnis3);
+
+let e = 7.4;
+let f = 7.5;
+
+
+result = e + f;
+console.log(result);
+
+result2 = (c + e) * c;
+console.log(result2)
+
+result3 = (c - b) * a;
+console.log(result3)
+
+// 02 Texte SREINGS
+
+let Name = "Sam";
+console.log(Name);
+
+result2 = (c + e) * c;
+console.log(result2)
+
+result3 = (c - b) * a;
+console.log(result3)
+
+
+result4 = d + e;
+console.log("das Ergebnis von " + d + " und " + e + " ist " + result4);
+
+//03 Aufgabe zu Variablen 
+
+let banana = "Bananaf afd asdf xy";
+let apple = "Apple";
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.42;
+let Kilo = 1
+let DGbanana = 0.22;
+let DGapple = 0.34;
+let applePerKilo = Kilo / DGapple;
+let bananaPerKilo = Kilo / DGbanana;
+
+let PriceApple = applePricePerKilo / applePerKilo;
+console.log("Eine Apfel kostet " + PriceApple);
+
+let PriceBanana = bananaPricePerKilo / bananaPerKilo;
+console.log("Eine Banane kostet " + PriceBanana);
+
+
+Ergebnis5 = (Kilo / DGbanana);
+console.log("Ein Kilo sind " + Ergebnis5 + " Bananen ")
+
+Ergebnis6 = (Kilo / DGapple);
+console.log("Ein Kilo sind " + Ergebnis6 + " Apple ")
+
+Ergebnis7 = PriceApple * 8;
+console.log("8 Äpfel kosten " + Ergebnis7)
+
+Ergebnis8 = PriceBanana * 17;
+console.log("17 Bananen kosten " + Ergebnis8);
+
+Ergebnis9 = applePricePerKilo * 1000
+console.log("Eine Tonne Äpfel kosten " + Ergebnis9);
+
+Ergebnis10 = bananaPricePerKilo * 1000
+console.log("Eine Tonne Bananen kosten " + Ergebnis10);
