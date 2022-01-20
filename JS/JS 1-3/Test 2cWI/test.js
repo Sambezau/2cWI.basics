a54654750d8ba5fe696c8cd4a96493fed41a9099
Samuel <samuel.meusburger@student.htldornbirn.at>
2022-01-20 14:25:23 +0100

    Test js finished

---
+++ JS/JS 1-3/Test 2cWI/test.js
@@ -0,0 +1,13 @@
+console.log("Ich zähle fon 1 bis 8");
+
+for (let count = 1; count <= 8; count++) {
+    console.log(count);
+}
+console.log("Das wars!");
+
+
+console.log("Ich zähle in Zweier-Schritten Rückwerts");
+
+for (let count = 11; count >= -3; count = count - 2) {
+    console.log(count)
+}
