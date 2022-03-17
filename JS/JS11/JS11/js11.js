0d6f144242575bbcca1722d42e8d889d8982a7e3
Samuel <samuel.meusburger@student.htldornbirn.at>
2022-03-17 13:54:13 +0100

    js11 finished

---
+++ JS/JS11/JS11/js11.js
@@ -0,0 +1,49 @@
+var res = add(12, 32);
+
+
+
+function add(a, b) {
+
+    var result = a + b;
+
+    return result;
+
+}
+
+console.log(res);
+
+
+
+// subtract(a, b)
+
+var res = subtract(45, 23);
+
+
+
+function subtract(a, b) {
+
+    var result = a - b;
+
+    return result;
+
+}
+
+console.log(res);
+
+
+
+// multiply(a, b)
+
+var res = multiply(21, 43);
+
+
+
+function multiply(a, b) {
+
+    var result = a * b;
+
+    return result;
+
+}
+
+console.log(res);
