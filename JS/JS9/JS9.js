549d56fdd6b7fd03d22fd19775735abe4a1136d3
Samuel <samuel.meusburger@student.htldornbirn.at>
2022-04-21 14:47:03 +0200

    Kalender

--- JS/JS9/JS9.js
+++ JS/JS9/JS9.js
@@ -1,1 +1,34 @@
-var =   
+let output = "";
+let isX = true
+for (let index = 0; index < 20; index++) {
+    if (index % 4 == 0) {
+        console.log(output);
+        output = ""
+        if (isX == true) {
+            isX = false
+        } else {
+            isX = true;
+        }
+
+
+
+    }
+    if (isX == true) {
+        output = output + "X";
+
+
+
+    } else {
+        output = output + "0";
+    }
+}
+
+//Aufgabe 09
+var sum = 0
+for (let index = 0; index < 100; index++) {
+    if (index % 2 == 0) {
+        sum += index
+    }
+}
+console.log(sum);
+
